be086b69e6226aea6140d6a6bae59fac2839e8f6
Dadhipriya Naik <nayakpriya056@gmail.com>
2024-12-11 12:24:54 +0530

    This is OS module

---
+++ Backend/Backend/class-3/os.js
@@ -0,0 +1,11 @@
+//import modules
+const os = require("os");
+
+console.log(os);
+console.log(os.homedir()); //c:\user\hp
+console.log(os.hostname()); //DESKTOP-JB47NQB
+console.log(os.platform()); //win32
+console.log(os.release()); //10.0.26100
+console.log(os.userInfo().username);
+console.log(os.totalmem());
+console.log(os.type());
